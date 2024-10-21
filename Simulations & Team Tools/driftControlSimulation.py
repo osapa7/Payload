a986b8836989b531167f7bff6cefa78f3ba9de2d
ChenJeff123 <143658756+ChenJeff123@users.noreply.github.com>
2024-10-21 12:12:17 -0400

    added functionality for graphing multiple different starting points

--- Simulations & Team Tools/driftControlSimulation.py
+++ Simulations & Team Tools/driftControlSimulation.py
@@ -2,77 +2,65 @@ import random
 import math
 import matplotlib.pyplot as plt
 import numpy as np
+import copy
 
 # Define constants
-K = 2  # Motor constant (can be adjusted for different simulations)
+K = 2  # Motor constant
 KP = 8.96  # Proportional gain for PD controller
 KD = 16  # Derivative gain for PD controller
-VS = 5  # Vehicle speed (can be adjusted for initial testing)
+VS = 5  # Vehicle speed
 DT = 0.01
 RAND = 50/DT
 
-ang_vel = 0
-xvel = [0]
-yvel = [0]
+target = (0,5)
 
 # Function to generate wind vector in polar form
-def generate_wind(a, b, c, d, e, f, dt, wind_mag):
-  wind_mag_change = (a * random.uniform(-1, 1) + b) / dt
-  wind_mag += wind_mag_change * dt
+def generate_wind(a, b, c, d, e, f, wind_mag):
+  wind_mag_change = (a * random.uniform(-1, 1) + b) / DT
+  wind_mag += wind_mag_change * DT
   wind_mag = max(c, min(wind_mag, d))  # Limit magnitude
   wind_angle = e + (f - e) * random.uniform(0, 1)  # Limit angle
   wind_angle = wind_angle % 360  # Wrap around to 0-360 degrees
   return wind_mag, wind_angle
 
 # Function to calculate goal angle
-def calculate_goal_angle(position, target, wind_mag, wind_angle, correction):
-  global xvel
-  global yvel
-  dx = target[0] - position[0][-1]
-  dy = target[1] - position[1][-1]
+def calculate_goal_angle(pos, wind_mag, wind_angle, correction):
+  global target
+
+  # distance from current to target
+  dx = target[0] - pos[0][-1]
+  dy = target[1] - pos[1][-1]
 
   wind = np.array([wind_mag * math.cos(math.radians(wind_angle)), wind_mag * math.sin(math.radians(wind_angle))])
 
-  print("Wind: ", wind)
-
   return_angle = math.degrees(math.atan2(dy, dx)) % 360
 
+  # wind correction logic
   if correction == True:
-
     direction = np.array([dx, dy])
-
-    print("Direction: ", direction)
-
     direction_norm = np.linalg.norm(direction)
 
     wind_todirection = direction * np.dot(wind, direction) / direction_norm**2
-
     wind_cross = wind - wind_todirection
 
     h_prime = direction/direction_norm * np.sqrt(VS**2 - np.linalg.norm(wind_cross)**2)
 
     goal_vec = h_prime - wind_cross
 
-    xvel.append(goal_vec[0])
-    yvel.append(goal_vec[1])
-
     goal_angle = math.degrees(math.atan2(goal_vec[1], goal_vec[0])) % 360
-
-    print(goal_angle, return_angle)
 
     return_angle = goal_angle
 
   return return_angle
 
-# System function (simulates PD controller)
-def system(pos, targ, heading, past_error, dt, wind_mag, wind_angle, correction):
-
+# Function simulates PD controller
+def system(pos, heading, past_error, wind_mag, wind_angle, correction):
   # Calculate goal angle
-  goal_angle = calculate_goal_angle(pos, targ, wind_mag, wind_angle, correction)
+  goal_angle = calculate_goal_angle(pos, wind_mag, wind_angle, correction)
 
   # Calculate error terms
   angle_error = goal_angle - heading
-  derror = (angle_error - past_error)/dt
+  derror = (angle_error - past_error)/DT
 
   # Calculate control signal
   pwm = KP * angle_error + KD * derror
@@ -80,70 +68,111 @@ def system(pos, targ, heading, past_error, dt, wind_mag, wind_angle, correction)
   return pwm, angle_error
 
 
-# Update state function
-def update_state(pwm, dt):
-  global ang_vel
+# Updates heading and angular velocity
+def update_state(ang_vel, index, pwm):
   accel = K * pwm
   accel += RAND * random.uniform(-1, 1)
-  ang_vel += accel * dt
+  ang_vel[index] += accel * DT
 
-  return ang_vel * dt
+  return ang_vel[index] * DT
 
+# updates the heading and position
+def move(pos, heading, total_time, ang_vel, xvel, yvel, past_error, completed, wind_mag, wind_angle, correction):
+  global target
+
+  # iterates through each starting point
+  for i in range(len(pos)):
+    if i not in completed:
+      total_time[i] += 1
+      # Get system output
+      pwm, past_error[i] = system(pos[i], heading[i][-1], past_error[i], wind_mag, wind_angle, correction)
+
+      # Update state
+      heading[i].append(heading[i][-1] + update_state(ang_vel, i, pwm))
+
+      xvel[i].append(VS * math.cos(math.radians(heading[i][-1])) + wind_mag * math.cos(math.radians(wind_angle)))
+      yvel[i].append(VS * math.sin(math.radians(heading[i][-1])) + wind_mag * math.sin(math.radians(wind_angle)))
+      pos[i][0].append(pos[i][0][-1] + xvel[i][-1] * DT)
+      pos[i][1].append(pos[i][1][-1] + yvel[i][-1] * DT)
+
+      if math.sqrt((pos[i][0][-1]-target[0])**2 + (pos[i][1][-1]-target[1])**2) < 0.1:
+        completed.append(i)
 
 # Simulation loop
-def simulate(target, steps, dt, rand_mag, offset, mag_lower, mag_upper, wind_lower, wind_upper, correction):
+def simulate(steps, rand_mag, offset, wind_mag_lower, wind_mag_upper, ang_lower, ang_upper):
   # Initialize state
-  heading = [0]
-  pos = [[0],[0]]
-  past_error = 0
   wind_mag = 0
-  total_time = 1
+
+  # not wind-corrected state
+  pos = [([-3],[1]), ([0],[0])]
+  heading = [[0] for _ in pos]
+  xvel = [[0] for _ in pos]
+  yvel = [[0] for _ in pos]
+  total_time = [1] * len(pos)
+  ang_vel = [0] * len(pos)
+  past_error = [0] * len(pos)
+  completed = []
+
+  # wind-corrected state
+  posw = copy.deepcopy(pos)
+  headingw = copy.deepcopy(heading)
+  xvelw = copy.deepcopy(xvel)
+  yvelw = copy.deepcopy(yvel)
+  total_timew = [1] * len(pos)
+  ang_velw = [0] * len(pos)
+  past_errorw = [0] * len(pos)
+  completedw = []
       
   # Simulation loop
   for _ in range(int(steps)):
-    total_time += 1
     # Generate wind
-    wind_mag, wind_angle = generate_wind(rand_mag, offset, mag_lower, mag_upper, wind_lower, wind_upper, dt, wind_mag)
+    wind_mag, wind_angle = generate_wind(rand_mag, offset, wind_mag_lower, wind_mag_upper, ang_lower, ang_upper, wind_mag)
 
-    # Get system output
-    pwm, past_error = system(pos, target, heading[-1], past_error, dt, wind_mag, wind_angle, correction)
+    # the non-wind-corrected simulation
+    move(pos, heading, total_time, ang_vel, xvel, yvel, past_error, completed, wind_mag, wind_angle, False)
 
-    # Update state
-    heading.append(heading[-1] + update_state(pwm, dt))
-    
+    # wind-corrected simulation
+    move(posw, headingw, total_timew, ang_velw, xvelw, yvelw, past_errorw, completedw, wind_mag, wind_angle, True)
+      
 
-    pos[0].append(pos[0][-1] + (VS * math.cos(math.radians(heading[-1])) + wind_mag * math.cos(math.radians(wind_angle))) * dt)
-    pos[1].append(pos[1][-1] + (VS * math.sin(math.radians(heading[-1])) + wind_mag * math.sin(math.radians(wind_angle))) * dt)
+  # displays all graphs for each starting point
+  for i in range(len(pos)):
+    plt.figure()
+    plt.title(f"Position of Starting Point ({pos[i][0][0]}, {pos[i][1][0]})")
+    plt.plot(pos[i][0], pos[i][1], ls='-', color='pink', label='non-corrected')
+    plt.plot(posw[i][0], posw[i][1], ls='-', color='red', label='wind corrected')
+    plt.legend()
 
-    if math.sqrt((pos[0][-1]-target[0])**2 + (pos[1][-1]-target[1])**2) < 0.1:
-      break
-  
-  return pos, heading, total_time
+    plt.figure()
+    plt.subplot(2, 1, 1) 
+    plt.title("X Velocity")
+    plt.plot(np.arange(0, (total_time[i])*DT, DT), xvel[i], ls='-', color='orange', label='non-corrected')
+    plt.plot(np.arange(0, (total_timew[i])*DT, DT), xvelw[i], ls='-', color='brown', label='wind corrected')
+    plt.legend()
+
+    plt.subplot(2, 1, 2) 
+    plt.title("Y Velocity")
+    plt.plot(np.arange(0, (total_time[i])*DT, DT), yvel[i], ls='-', color='olive', label='non-corrected')
+    plt.plot(np.arange(0, (total_timew[i])*DT, DT), yvelw[i], ls='-', color='green', label='wind corrected')
+    plt.tight_layout()
+    plt.legend()
+
+    plt.figure()
+    plt.title(f"Heading of Starting Point ({pos[i][0][0]}, {pos[i][1][0]})")
+    plt.plot(np.arange(0,(total_time[i])*DT,DT), heading[i], ls = '-', color='cyan', label='non-corrected')
+    plt.plot(np.arange(0,(total_timew[i])*DT,DT), headingw[i], ls = '-', color='blue', label='wind corrected')
+    plt.legend()
+    plt.show()
 
 
-pos1, heading1, total_time1 = simulate([0,5], 10/DT, DT, 0.1, 0, 0, 2, 90, 180, True)
-pos2, heading2, total_time2 = simulate([0,5], 10/DT, DT, 0.1, 0, 0, 2, 90, 180, False)
+# wind variables
+rand_mag = 0.1
+offset = 0
+w_min = 0
+w_max = 3
+angle_min = 90
+angle_max = 180
 
+time = 10
 
-plt.figure()
-plt.title("Position")
-plt.plot(pos1[0], pos1[1], ls='-', color='red', label='wind corrected')
-plt.plot(pos2[0], pos2[1], ls='-', color='pink', label='non-corrected')
-plt.legend()
-
-plt.figure()
-plt.subplot(2, 1, 1)  # Create subplot 1 (top row)
-plt.title("X Velocity")
-plt.plot(np.arange(0, (total_time1)*DT, DT), xvel, ls='-', color='orange')
-
-plt.subplot(2, 1, 2)  # Create subplot 2 (bottom row)
-plt.title("Y Velocity")
-plt.plot(np.arange(0, (total_time1)*DT, DT), yvel, ls='-', color='green')
-plt.tight_layout()
-
-plt.figure()
-plt.title("Heading")
-plt.plot(np.arange(0,(total_time1)*DT,DT), heading1, ls = '-', color='blue', label='wind corrected')
-plt.plot(np.arange(0,(total_time2)*DT,DT), heading2, ls = '-', color='cyan', label='non-corrected')
-plt.legend()
-plt.show()
+simulate(time/DT, rand_mag, offset, w_min, w_max, angle_min, angle_max)
